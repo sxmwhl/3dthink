37b3bd1c65d8658e209a6ee377aa78fe5c661110
sxmwhl <lvxin.rd@gmail.com>
2015-01-15 22:16:26 +0800

    添加更换预览图表格

--- Application/Home/Controller/FormController.class.php
+++ Application/Home/Controller/FormController.class.php
@@ -158,6 +158,21 @@ class FormController extends Controller {
     	$model_show['hl_on']=$model_show['hl_on']==0?"false":"true";
     	$model_show['dl_on']=$model_show['dl_on']==0?"false":"true";
     	$this->model=$model_show;
+    	$this->title="更换模型".$model_show['title']."缩略图";
     	$this->display('preview');
+    }
+    public function change(){
+    	$upload = new \Think\Upload();// 实例化上传类
+    	$upload->maxSize = 3145728 ;// 设置附件上传大小
+    	$upload->exts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+    	$upload->rootPath = __ROOT__.'Public/'; // 设置附件上传根目录
+    	$upload->autoSub  = false;
+    	$upload->savePath = 'upload/'; // 设置附件上传（子）目录
+    	// 上传文件
+    	$info = $upload->upload();
+    	if(!$info) {// 上传错误提示错误信息
+    		$this->error($upload->getError());
+    	}else{// 上传成功 获取上传文件信息
+    	}
     }
 }
