413a77e7cea2bf1ced064b050ad84e7deffe9ffc
sxmwhl <lvxin.rd@gmail.com>
2015-01-30 23:19:40 +0800

    加入accept验证
    继续改变bootstrap风格

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,8 +4,8 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
     	$Moxing=M('Moxing');
-    	$list1 = $Moxing->where('sign=0')->order('views desc')->limit(6)->select();
-    	$list2 = $Moxing->where('sign=0')->order('time_update desc')->limit(6)->select();
+    	$list1 = $Moxing->where('sign=0')->order('views desc')->limit(8)->select();
+    	$list2 = $Moxing->where('sign=0')->order('time_update desc')->limit(8)->select();
     	$this->models1=$list1;
     	$this->models2=$list2;
     	//echo $Moxing->getLastSql();
