be65487dce2a0d19f2a7cd94d78a345cf24d2ad5
sxmwhl <lvxin.rd@gmail.com>
2015-02-03 16:55:54 +0800

    配置信息修改

---
+++ Application/Common/Conf/config_ali.php
@@ -0,0 +1,27 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+		//数据库配置信息
+		'USER_ALLOW_REGISTER'   => true, // 用户注册
+		'WEB_SITE_CLOSE'   => true, // 站点开关
+		'DB_TYPE'   => 'mysql', // 数据库类型
+		'DB_HOST'   => 'qdm111421528.my3w.com', // 服务器地址
+		'DB_NAME'   => 'qdm111421528_db', // 数据库名
+		'DB_USER'   => 'qdm111421528', // 用户名
+		'DB_PWD'    => 'sxm206350482', // 密码
+		'DB_PORT'   => 3306, // 端口
+		'DB_PREFIX' => 'think_', // 数据库表前缀
+		'DB_CHARSET'=> 'utf8', // 字符集
+		'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
+	//'DB_DSN' => 'mysql://root@localhost:3306/3dshare#utf8'
+		'TMPL_PARSE_STRING'  =>array(
+				'__BCS_X3DFILE__' => 'http://bcs.duapp.com/x3dfile',
+		
+				//'__BCS_UPLOAD__' => 'http://bcs.duapp.com/x3dfile/upload',
+				'__BCS_UPLOAD__' => __ROOT__.'/Public/upload',
+				//'__BCS_PUBLIC__' => 'http://bcs.duapp.com/x3dfile/public',
+				'__BCS_PUBLIC__' => __ROOT__.'/Public',
+				/*********本地、远程切换完毕**************/
+				'__SITENAME__' => 'web3d模型分享',
+		),
+);
---
+++ Application/install/index_ali.php
@@ -0,0 +1,92 @@
+<?php
+header("Content-Type: text/html;charset=utf-8"); 
+$con= new mysqli("qdm111421528.my3w.com","qdm111421528","sxm206350482");
+$con->select_db("qdm111421528_db");
+$sqls = "
+DROP TABLE IF EXISTS `think_moxing`;
+CREATE TABLE think_moxing
+(
+id int unsigned NOT NULL AUTO_INCREMENT, 
+PRIMARY KEY(id),
+uid int unsigned NOT NULL DEFAULT '0',
+title varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci ,
+description varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci ,
+preview_ext varchar(4),
+folder varchar(32),
+category smallint(5) unsigned NOT NULL DEFAULT '0',
+creator varchar(30),
+email varchar(30),
+time_update datetime,
+sign tinyint unsigned,
+views mediumint unsigned,
+hl_on boolean NOT NULL DEFAULT '0',
+dl_on boolean NOT NULL DEFAULT '0',
+vp_position varchar(30),
+vp_orientation varchar(40),
+ip_upload varchar(15),
+ip_last_modify varchar(15)
+);
+		
+DROP TABLE IF EXISTS `think_category`;
+CREATE TABLE think_category 
+(
+  `cate_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `root_id` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `cate_name` varchar(50) NOT NULL DEFAULT '',
+  `cate_isbest` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `cate_order` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `cate_level` smallint(5) unsigned NOT NULL DEFAULT '1',
+  `cate_keywords` varchar(100) NOT NULL DEFAULT '',
+  `cate_description` varchar(255) NOT NULL DEFAULT '',
+  `cate_arrparentid` varchar(255) NOT NULL DEFAULT '',
+  `cate_childcount` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `cate_postcount` smallint(5) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`cate_id`),
+  KEY `root_id` (`root_id`)
+);
+		
+DROP TABLE IF EXISTS `think_ucenter_member`;
+CREATE TABLE `think_ucenter_member` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `username` char(16) NOT NULL COMMENT '用户名',
+  `password` char(32) NOT NULL COMMENT '密码',
+  `email` char(32) NOT NULL COMMENT '用户邮箱',
+  `mobile` char(15) NOT NULL DEFAULT '' COMMENT '用户手机',
+  `reg_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '注册时间',
+  `reg_ip` bigint(20) NOT NULL DEFAULT '0' COMMENT '注册IP',
+  `last_login_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '最后登录时间',
+  `last_login_ip` bigint(20) NOT NULL DEFAULT '0' COMMENT '最后登录IP',
+  `update_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+  `status` tinyint(4) DEFAULT '0' COMMENT '用户状态',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  KEY `status` (`status`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='用户表';
+		
+DROP TABLE IF EXISTS `think_member`;
+CREATE TABLE `think_member` (
+  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `nickname` char(16) NOT NULL DEFAULT '' COMMENT '昵称',
+  `sex` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '性别',
+  `birthday` date NOT NULL DEFAULT '0000-00-00' COMMENT '生日',
+  `qq` char(10) NOT NULL DEFAULT '' COMMENT 'qq号',
+  `score` mediumint(8) NOT NULL DEFAULT '0' COMMENT '用户积分',
+  `login` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '登录次数',
+  `reg_ip` bigint(20) NOT NULL DEFAULT '0' COMMENT '注册IP',
+  `reg_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '注册时间',
+  `last_login_ip` bigint(20) NOT NULL DEFAULT '0' COMMENT '最后登录IP',
+  `last_login_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '最后登录时间',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '会员状态',
+  PRIMARY KEY (`uid`),
+  KEY `status` (`status`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='会员表';";
+if ($con->multi_query($sqls))
+{
+	echo "数据库安装完毕！！！";
+	}else{
+
+echo "数据库安装出错！！！";
+	}
+$con->close();
+?>
