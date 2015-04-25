c35caa3f6888678a69ec5668779682b1b1af2fcd
xiaoming <lvxin.rd@gmail.com>
2015-04-25 12:04:25 +0800

    首页增加现实diy创作者

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -14,6 +14,9 @@ class IndexController extends Controller {
     	$this->categories=$list3;
     	$Diy=M('Diy');
     	$list4=$Diy->where('status=1')->order('views desc')->limit(8)->select();
+    	for ($i= 0;$i< count($list4); $i++){
+    		$list4[$i]['user']=get_username($list4[$i]['uid']);
+    	}
     	$this->diys=$list4;
     	$this->title='首页';
     	$this->keywords='模型分享,3d模型,web3d,x3d模型,三维网站,三维模型';
