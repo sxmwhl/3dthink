7104787ac0e141a7a502a7e14490b20b6c3df9f2
xiaoming <lvxin.rd@gmail.com>
2015-04-25 19:33:23 +0800

    自动生成sitemap

--- Application/Home/Controller/AdminController.class.php
+++ Application/Home/Controller/AdminController.class.php
@@ -102,7 +102,128 @@ class AdminController extends Controller {
     	if($id==0)$this->error('参数错误！','index');
     	$Member=M('Member');
     	$did=$Member->where('uid='.$id)->delete();
-    	$this->success('删除成功！');
-    	
+    	$this->success('删除成功！');    	
     }
+    public function sitemap(){
+    	$content='<?xml version="1.0" encoding="UTF-8"?>'.chr(10);
+    	$content.="<urlset>\n";
+    	
+    	$data_array=array(
+    			array(
+    					'loc'=>'http://www.3dant.cn/',
+    					'priority'=>'1.0',
+    					'lastmod'=>date(DATE_W3C),
+    					'changefreq'=>'always'
+    			),
+    			array(
+    					'loc'=>'http://www.3dant.cn/show/',
+    					'priority'=>'0.9',
+    					'lastmod'=>date(DATE_W3C),
+    					'changefreq'=>'always'
+    			),
+    			array(
+    					'loc'=>'http://www.3dant.cn/index.php/Home/Help/index',
+    					'priority'=>'0.9',
+    					'lastmod'=>date(DATE_W3C),
+    					'changefreq'=>'daily'
+    			),
+    			array(
+    					'loc'=>'http://www.3dant.cn/index.php/Home/Help/about',
+    					'priority'=>'0.5',
+    					'lastmod'=>date(DATE_W3C),
+    					'changefreq'=>'daily'
+    			),
+    			array(
+    					'loc'=>'http://www.3dant.cn/index.php/Home/Help/browserSupport',
+    					'priority'=>'0.5',
+    					'lastmod'=>date(DATE_W3C),
+    					'changefreq'=>'monthly'
+    			),
+    			array(
+    					'loc'=>'http://www.3dant.cn/index.php/Home/Help/navigation',
+    					'priority'=>'0.6',
+    					'lastmod'=>date(DATE_W3C),
+    					'changefreq'=>'monthly'
+    			),
+    			array(
+    					'loc'=>'http://www.3dant.cn/index.php/home/user/login',
+    					'priority'=>'0.4',
+    					'lastmod'=>date(DATE_W3C),
+    					'changefreq'=>'monthly'
+    			),
+    			array(
+    					'loc'=>'http://www.3dant.cn/index.php/home/user/register',
+    					'priority'=>'0.4',
+    					'lastmod'=>date(DATE_W3C),
+    					'changefreq'=>'monthly'
+    			)    			
+    	);
+    	$Moxing=M('Moxing');
+    	$list=$Moxing->select();
+    	foreach ($list as $m){
+    		$data_array=array_merge($data_array,array(
+    				array(
+    						'loc'=>'http://www.3dant.cn/index.php/Home/Index/model?f='.$m['folder'],
+    						'priority'=>'0.8',
+    						'lastmod'=>date(DATE_W3C,$m['time_update']),
+    						'changefreq'=>'daily'    						    						
+    				),
+    				array(
+    						'loc'=>'http://www.3dant.cn/index.php/Home/Index/modelIn?f='.$m['folder'],
+    						'priority'=>'0.5',
+    						'lastmod'=>date(DATE_W3C,$m['time_update']),
+    						'changefreq'=>'daily'
+    				)
+    		));
+    	}
+    	$Diy=M('Diy');
+    	$list=$Diy->select();
+    	foreach ($list as $m){
+    		$data_array=array_merge($data_array,array(
+    				array(
+    						'loc'=>'http://www.3dant.cn/index.php/Home/Index/diy?id='.$m['id'],
+    						'priority'=>'0.8',
+    						'lastmod'=>date(DATE_W3C,$m['time_update']),
+    						'changefreq'=>'daily'
+    				),
+    				array(
+    						'loc'=>'http://www.3dant.cn/index.php/Home/Index/diyIn?f='.$m['id'],
+    						'priority'=>'0.5',
+    						'lastmod'=>date(DATE_W3C,$m['time_update']),
+    						'changefreq'=>'daily'
+    				)
+    		));
+    	}
+    	$Category=M('Category');
+    	$list=$Category->select();
+    	foreach ($list as $m){
+    		$data_array=array_merge($data_array,array(
+    				array(
+    						'loc'=>'http://www.3dant.cn/index.php/Home/category?cate='.$m['cate_id'],
+    						'priority'=>'0.8',
+    						'lastmod'=>date(DATE_W3C,$m['time_update']),
+    						'changefreq'=>'weekly'
+    				)
+    		));
+    	}
+    	foreach($data_array as $data){
+    		$content.=$this->create_item($data);
+    		$i++;
+    	}
+    	$content.='</urlset>';
+    	$fp=fopen('sitemap.xml','w+');
+    	fwrite($fp,$content);
+    	fclose($fp);  
+    	$this->success('成功生成'.$i."条记录！",'index');  	
+    }
+   private function create_item($data){
+    	$item="<url>\n";
+    	$item.="<loc>".$data['loc']."</loc>\n";
+    	$item.="<priority>".$data['priority']."</priority>\n";
+    	$item.="<lastmod>".$data['lastmod']."</lastmod>\n";
+    	$item.="<changefreq>".$data['changefreq']."</changefreq>\n";
+    	$item.="</url>\n";
+    	return $item;
+    }
+    
 }
